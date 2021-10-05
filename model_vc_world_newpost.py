90892d4e12822caed093b8d1b3e15d54c9783f88
SuzukiDaishi <zukky.rikugame@gmail.com>
2021-10-05 16:53:48 +0900

    new post

--- model_vc_world_newpost.py
+++ model_vc_world_newpost.py
@@ -119,6 +119,38 @@ class PostnetSE(nn.Module):
         x = self.convolutions[-1](x)
         x = x.squeeze(1)
         return x
+
+class Generator(nn.Module):
+    """Generator network."""
+    def __init__(self, dim_neck, dim_emb, dim_pre, freq):
+        super(Generator, self).__init__()
+        
+        self.style_encoder = StyleEncoder(dim_emb)
+        self.encoder = ContentEncoder(dim_neck, dim_emb, freq)
+        self.decoder = Decoder(dim_neck, dim_emb, dim_pre)
+        self.postnet = PostnetSE()
+
+    def forward(self, x, c_org, c_trg):
+        codes = self.encoder(x, c_org)
+        if c_trg is None:
+            return torch.cat(codes, dim=-1)
+        
+        tmp = []
+        for code in codes:
+            tmp.append(code.unsqueeze(1).expand(-1,int(x.size(1)/len(codes)),-1))
+        code_exp = torch.cat(tmp, dim=1)
+        
+        encoder_outputs = torch.cat((code_exp, c_trg.unsqueeze(1).expand(-1,x.size(1),-1)), dim=-1)
+        
+        mel_outputs = self.decoder(encoder_outputs)
+                
+        mel_outputs_postnet = self.postnet(mel_outputs.transpose(2,1))
+        mel_outputs_postnet = mel_outputs + mel_outputs_postnet.transpose(2,1)
+        
+        #mel_outputs = mel_outputs.unsqueeze(1)
+        #mel_outputs_postnet = mel_outputs_postnet.unsqueeze(1)
+        
+        return mel_outputs, mel_outputs_postnet, torch.cat(codes, dim=-1)
     
 if __name__ == '__main__':
     model1 = Postnet()
